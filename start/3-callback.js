65a1f293d4e3a47077981696d3ce950d673bd394
nirkaufman <nirkaufman@gmail.com>
2020-12-27 13:44:16 -0500

    more code on collection

--- start/3-callback.js
+++ start/3-callback.js
@@ -1,43 +1,42 @@
-
 // collection is an object with getValues
-const collection = {
-  getValues: function (callbackFn) {
-    const items = [1,2,3,4,5,6];
-    for(let item of items) {
-      callbackFn(item);
+function createCollection(getValues) {
+  return {
+    getValues,
+    manipulate: function (manipulateFn) {
+      return manipulateFn(this)
     }
-  },
-  manipulate: function (manipulateFn) {
-    return manipulateFn(this)
   }
 }
 
+// create collection from array
+function fromArray(array) {
+  return createCollection(function (callback) {
+    for(let item of array) {
+      callback(item);
+    }
+  })
+}
+
+
 function map(mapFn) {
   return function (inputCollection) {
-    return {
-      getValues: function (callbackFn) {
-        inputCollection.getValues( value => callbackFn(mapFn(value)) )
-      },
-      manipulate: function (manipulateFn) {
-        return manipulateFn(this)
-      }
-    }
+    return createCollection(function (callbackFn) {
+      inputCollection.getValues( value => callbackFn(mapFn(value)))
+    })
   }
 }
 function filter(filterFn) {
   return function (inputCollection) {
-    return {
-      getValues: function (callbackFn) {
-        inputCollection.getValues( value => filterFn(value) && callbackFn(value))
-      },
-      manipulate: function (manipulateFn) {
-        return manipulateFn(this)
-      }
-    }
+    return createCollection(function(callbackFn){
+      inputCollection.getValues( value => filterFn(value) && callbackFn(value))
+    })
   }
 }
 
-collection
-    .manipulate(map( v => v * 10 ))
-    .manipulate(filter( v => v === 20))
+
+const numbers = fromArray([1,2,3,4,5]);
+
+numbers
+    .manipulate(map(v => v *10))
+    .manipulate(filter(v => v === 10))
     .getValues( v => console.log(v))
